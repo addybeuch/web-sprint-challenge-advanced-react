654f113f09c1b865337644bebb740e46f921a7a3
addybeuch <addybeuch@gmail.com>
2021-10-01 02:28:03 -0600

    Almost completed

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,31 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+    render(<CheckoutForm/>)
+});
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", () => {
+    render(<CheckoutForm/>)
+    const firstName = screen.getByLabelText(/First Name:/i);
+    const lastName = screen.getByLabelText(/Last Name:/i);
+    const address = screen.getByLabelText(/Address:/i);
+    const city = screen.getByLabelText(/City:/i);
+    const state = screen.getByLabelText(/State:/i);
+    const zip = screen.getByLabelText(/Zip:/i);
+    userEvent.type(firstName, 'Brennan');
+    userEvent.type(lastName, 'Smith');
+    userEvent.type(address, '1234 West 1200 South');
+    userEvent.type(city, 'Salt Lake City');
+    userEvent.type(state, 'UT');
+    userEvent.type(zip, '12345');
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    const success = screen.queryByText(/You have ordered some plants!/)
+    expect(success).toBeInTheDocument;
+});
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,21 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+const useLocalStorage = (key, initialValues) => {
+    const [values, setValues] = useState(()=> {
+        if (localStorage.getItem(key)) {
+            return(JSON.parse(localStorage.getItem(key)));
+        } else {
+            localStorage.setItem(key, JSON.stringify(initialValues));
+            return(initialValues);
+        }
+    });
+
+    const setStoredValues = (values) => {
+        localStorage.setItem(key, JSON.stringify(values));
+        setValues(values);
+    }
+    return [values, setStoredValues];
+}
+
+export default useLocalStorage;
