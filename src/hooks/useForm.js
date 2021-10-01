4fc51b7f73b8808c2fe5b1e77a9cf94a20153753
addybeuch <addybeuch@gmail.com>
2021-10-01 13:10:05 -0600

    Bug fixed

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,7 +1,7 @@
 // write your custom hook here to control your checkout form
 import { useState } from 'react';
 
-useForm = (initialValue) => {
+const useForm = (initialValue) => {
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
     const [values, setValues] = useState(initialValue);
   
